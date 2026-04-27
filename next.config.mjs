5b0ea11c2ded527c2636a9b9a2bbac039993a794
yyubin <hazing120@gmail.com>
2026-04-27 13:27:07 +0900

    chore: update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'picsum.photos' },
+      {
+        protocol: 'https',
+        hostname: 'image.msscdn.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
