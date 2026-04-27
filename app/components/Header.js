b02c2d44db6433b331a4ac739100baec590d3171
yyubin <hazing120@gmail.com>
2026-04-27 10:30:58 +0900

    fix: userStore에 저장되지 않던 문제 해결

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,6 +5,7 @@ import { useUiStore } from '@/store/useUiStore';
 import { useUserStore } from '@/store/useUserStore';
 import Link from 'next/link';
 import { useState } from 'react'
+import { redirect } from 'next/navigation';
 
 const NAV = ['신상품', '남성', '여성', '아우터', '상의', '하의', '신발', '브랜드', '세일']
 
@@ -27,6 +28,14 @@ export default function Header() {
         escapeClosable: true,
       }
     );
+  }
+
+  const restrictWithoutLogin = () => {
+    if (!user) {
+      alert("로그인을 해주세요");
+      return;
+    }
+    redirect("/mypage");
   }
 
 
@@ -77,14 +86,14 @@ export default function Header() {
 
             {!user && <button className="hidden md:block text-sm font-medium hover:text-gray-300" onClick={handleLogin}>로그인</button>}
             {user && <button className="hidden md:block text-sm font-medium hover:text-gray-300" onClick={logout}>로그아웃</button>}
-            <a href="/mypage" className="hidden md:block text-sm font-medium hover:text-gray-300">마이페이지</a>
+            <a className="hidden md:block text-sm font-medium hover:text-gray-300" onClick={restrictWithoutLogin}>마이페이지</a>
           </div>
         </div>
 
         {/* 모바일 카테고리 스크롤 */}
         <div className="md:hidden flex gap-4 overflow-x-auto pb-2 text-sm font-medium scrollbar-none">
-          {NAV.map((item) => (
-            <a key={item} href="#" className="whitespace-nowrap hover:text-gray-300">{item}</a>
+          {NAV.map((item, index) => (
+            <a key={item} href={`/category/${index + 1}`} className="whitespace-nowrap hover:text-gray-300">{item}</a>
           ))}
         </div>
       </div>
